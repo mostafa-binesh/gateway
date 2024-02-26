57ba4fa56e02ff5423342758cb7bf84458f59e07
Mostafa <ubirockteam@gmail.com>
2024-02-26 20:36:04 +0330

    fix: clean AddGatewayConfigIdToUsersTable migration

--- migrations/2024_02_26_165121_add_gateway_config_id_to_users_table.php
+++ migrations/2024_02_26_165121_add_gateway_config_id_to_users_table.php
@@ -19,29 +19,13 @@ class AddGatewayConfigIdToUsersTable extends Migration
     public function up()
     {
         Schema::table(self::getTable(), function (Blueprint $table) {
-            // $table->text('description')->after('ip')->nullable();
-            // $table->engine = "innoDB";
-            // $table->unsignedBigInteger('id', true);
-
             $table->unsignedBigInteger('gateway_config_id');
-            // $table->unsignedBigInteger('user_id');
 
             $table
                 ->foreign('gateway_config_id')
                 ->references('id')
-                ->on(CreateGatewayConfigsTable::getTable())
+                ->on($this->getTable())
                 ->onDelete('cascade');
-            // $table
-            //     ->foreign('user_id')
-            //     ->references('id')
-            //     ->on($this->getUserTable())
-            //     ->onDelete('cascade');
-            // $table->string('name');
-            // $table->text('config');
-            // $table->boolean('active');
-            // $table->enum('port', (array) Enum::getIPGs());
-            // $table->decimal('price', 15, 2);
-            // $table->string('tracking_code', 50)->nullable();
         });
     }
 
